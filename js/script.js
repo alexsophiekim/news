d95cb3a1666bf99a701767305b87d11f31de5738
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-08-15 19:25:53 +1200

    edited html/css/javascript for about page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-  console.log('ready');
+const homeBtn = $("#homeBtn")
+const aboutBtn = $("#aboutBtn");
+const container = $(".pageContainer");
+const aboutPage = $("#aboutPage");
+const newsPage = $("#newsPage");
+
+
+homeBtn.click(function(){
+  aboutPage.removeClass("d-block").addClass("d-none")
+  container.removeClass("bg-dark").addClass("bg-light");
+  newsPage.removeClass("d-none").addClass( "d-block" );
+});
+
+aboutBtn.click(function(){
+  newsPage.removeClass("d-block").addClass("d-none")
+  container.removeClass("bg-light, py-5").addClass("bg-dark");
+  aboutPage.removeClass("d-none").addClass( "d-block" );
+
 });
