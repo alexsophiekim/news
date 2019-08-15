249d53c4e6216e31068f109cc63b31594a87f0be
Sophie Kim <270010565@bun2-206-17.local>
2019-08-16 09:47:10 +1200

    fixed naming issue version 2 finalised

--- public/js/script.js
+++ public/js/script.js
@@ -46,11 +46,11 @@ const aboutPage = $("#aboutPage");
 homeBtn.click(function(){
   aboutPage.removeClass("d-block").addClass("d-none")
   container.removeClass("bg-dark").addClass("bg-light");
-  newsPage.removeClass("d-none").addClass( "d-block" );
+  newsArticleList.removeClass("d-none").addClass( "d-block" );
 });
 
 aboutBtn.click(function(){
-  newsPage.removeClass("d-block").addClass("d-none")
+  newsArticleList.removeClass("d-block").addClass("d-none")
   container.removeClass("bg-light, py-5").addClass("bg-dark");
   aboutPage.removeClass("d-none").addClass( "d-block" );
 
